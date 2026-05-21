306f6963000087d17e20abb0fb5a27f0f4f7ada5
MAR <marlariosc10@gmail.com>
2026-05-21 13:23:38 -0600

    Se agrega BD

--- Backend/BD.sql
+++ Backend/BD.sql
@@ -1,5 +1,8 @@
-CREATE BD CRM; 
-CREATE TABLE USERS,
-id_user int notnull,
-
+CREATE DATABASE CRM; 
+CREATE TABLE USERS(
+id_user int not null,
+name varchar(255) not null,
+email varchar(255) not null,
+password varchar(255) not null,
+PRIMARY KEY (id_user);      
       
--- FRONTEND/USERS/bd.sql
+++ FRONTEND/USERS/bd.sql
@@ -1,4 +1,4 @@
-CREATE database 'USERS'
+CREATE database USERS;
 
 
 CREATE TABLE USERS (
@@ -19,3 +19,10 @@ CREATE TABLE ticket (
 atendio_ticket varchar(50),
 
 )
+USE USERS;
+INSERT INTO USERS (id_user, name, email, password) VALUES (1, 'John Doe', 'john.doe@example.com', 'password123');
+INSERT INTO USERS (id_user, name, email, password) VALUES (2, 'Jane Smith', 'jane.smith@example.com', 'password456');
+
+use ticket;
+INSERT INTO ticket (id_folio, fecha, motivo, STATUS, atendio_ticket) VALUES (1, '2024-06-01', 'Problema con el producto', 'Abierto', 'John Doe');
+INSERT INTO ticket (id_folio, fecha, motivo, STATUS, atendio_ticket) VALUES (2, '2024-06-02', 'Consulta sobre el servicio', 'Cerrado', 'Jane Smith');   
